d599f84b93a6d20a8317609e8ab336f6c85d789c
JimmyLiujw <110658199+JimmyLiujw@users.noreply.github.com>
2022-08-18 15:51:39 +0800

    Update IRs.ts

--- IRs.ts
+++ IRs.ts
@@ -1,10 +1,10 @@
 // MakeCode blocks for sending commands using an IR LED
 
 /**
- * IRBlock
+ * IR Block
  */
-//% color=#0fbc11 icon="\u272a" block="IRBlock"
-//% category="IRBlock"
+//% color=#0fbc11 icon="\u272a" block="IR Block"
+//% category="IR Block"
 namespace makerbit_c {
   let irLed: InfraredLed;
 
